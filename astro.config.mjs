01c55e529a986ed51716296ad6831d85f97d4c0d
Colin <underscoredotspace@users.noreply.github.com>
2023-02-07 15:45:59 +0000

    add netlify plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,24 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-	server: { port: 1234 },
+	server: {
+		port: 1234,
+	},
 	integrations: [
 		tailwind({
 			config: {
 				applyBaseStyles: false,
 			},
 		}),
-		preact({ compat: true }),
+		preact({
+			compat: true,
+		}),
 		image(),
 	],
-
 	vite: {
 		define: {
 			'import.meta.env.APP_VERSION': JSON.stringify(process.env.npm_package_version),
@@ -32,4 +36,6 @@ export default defineConfig({
 			noExternal: ['@radix-ui/react-icons', '@fortawesome/react-fontawesome'],
 		},
 	},
+	output: 'server',
+	adapter: netlify(),
 });
