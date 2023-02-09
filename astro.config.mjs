653e3443c6840a806d94b785aa67df39fc052a59
Colin <underscoredotspace@users.noreply.github.com>
2023-02-09 18:19:40 +0000

    try node adapter instead remove logs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
-import netlify from '@astrojs/netlify/edge-functions';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: netlify(),
+	adapter: node({
+		mode: 'standalone',
+	}),
 	server: { port: 1234 },
 	integrations: [
 		tailwind({
