a25ee40975fcdf9ca118ee4c6aa68a5220a96e08
Colin <underscoredotspace@users.noreply.github.com>
2023-02-10 19:14:35 +0000

    remove ssr config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: netlify(),
 	server: { port: 1234 },
 	integrations: [
 		tailwind({
