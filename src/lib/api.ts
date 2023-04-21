32ddad0443b98a1c40a1d1a349b36f5812909497
Colin <underscoredotspace@users.noreply.github.com>
2023-04-21 14:18:49 +0000

    improve api functions

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,45 @@
-export const apiFetch = async <T extends any>(endpoint: string): Promise<T> => {
-	const url = `${import.meta.env.PUBLIC_API_URL}/${endpoint}`.replaceAll('//', '/');
+import { DateTime } from 'luxon';
+import type { Role, MemberCounts, MemberCountsAPI } from './types';
 
-	return await fetch(url).then((res) => res.json());
-};
+export const qs = (queryObject?: URLSearchParams): string =>
+	new URLSearchParams(queryObject).toString();
+
+const apiUrl = (endpoint: string, qs?: string) =>
+	`${import.meta.env.PUBLIC_API_URL}/${endpoint}${qs ? `?${qs}` : ''}`.replaceAll('//', '/');
+
+interface FetchJSONOptions {
+	query?: any;
+	body?: any;
+	method?: 'GET' | 'POST' | 'PATCH' | 'DELETE';
+}
+
+const fetchAPI = <T>(endpoint: string, { query, body, method }: FetchJSONOptions = {}) =>
+	fetch(apiUrl(endpoint, query ? qs(query) : undefined), {
+		method: method ?? 'GET',
+		credentials: 'include',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(body),
+	}).then(async (res) => {
+		if (res.status.toString().startsWith('2')) {
+			return res.json() as T;
+		}
+
+		throw new Error(await res.text());
+	});
+
+export async function getMembercount(): Promise<MemberCounts> {
+	const { PlayStation, Xbox, lastUpdated } = await fetchAPI<MemberCountsAPI>('member/count');
+
+	return {
+		PlayStation: Number(PlayStation),
+		Xbox: Number(Xbox),
+		lastUpdated: DateTime.fromJSDate(new Date(lastUpdated)),
+	};
+}
+
+export const getStaffProfiles = () =>
+	fetchAPI<Role[]>('profile/by-role', {
+		query: {
+			roleIds: ['546342033867014165', '549644467498516508', '568141992794783749'],
+		},
+	}).then((roles: Role[]) => roles.sort((a, b) => b.position - a.position));
---
+++ src/lib/types.ts
@@ -0,0 +1,65 @@
+import type { DateTime } from 'luxon';
+
+export interface Role {
+	id: string;
+	name: string;
+	color: string;
+	position: number;
+	members?: Member[];
+}
+
+export interface Member {
+	id: string;
+	username: string;
+	joined_at: Date;
+	displayName: string;
+	avatar?: string;
+	roles?: Role[];
+	profile?: Profile;
+}
+
+export interface Profile {
+	id: string;
+	name?: string;
+	location?: string;
+	bio?: string;
+	picture?: string;
+}
+
+export interface User {
+	id: string;
+	userid: string;
+	username: string;
+	avatar?: string;
+	expires: Date;
+}
+
+export interface Permission {
+	name: string;
+	roles?: Role[];
+}
+
+export interface ExtendedUser extends User {
+	member?: Member;
+	permissions?: string[];
+}
+
+export interface RoleListItem {
+	id: string;
+	name: string;
+	description?: string;
+	members?: Member[];
+	colour: string;
+}
+
+export type RoleListItems = RoleListItem[];
+
+export interface MemberCounts {
+	PlayStation: number;
+	Xbox: number;
+	lastUpdated: DateTime;
+}
+
+export interface MemberCountsAPI extends Omit<MemberCounts, 'lastUpdated'> {
+	lastUpdated: Date;
+}
